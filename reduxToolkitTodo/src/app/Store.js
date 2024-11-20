fdac44c3fa4d4e44bc945d2573e27612a3fafa58
Midhat786 <ksyedamidhat@gmail.com>
2024-11-19 18:18:02 -0800

    Delete reduxToolkitTodo directory

--- reduxToolkitTodo/src/app/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from "../features/todo/todoSlice"; // Import the reducer correctly
-
-export const store = configureStore({
-  reducer: {
-    todos: todoReducer, // Ensure the key 'todos' maps to the todoReducer
-  },
-});
